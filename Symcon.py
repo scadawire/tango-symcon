160844f8f1c7f34f3c4b9afcecce3f865c2b2464
Sebastian Jennen <sj@imagearts.de>
2024-08-22 14:36:48 +0200

    symcon: add update cache with interval based polling 8

--- Symcon.py
+++ Symcon.py
@@ -63,7 +63,7 @@ class Symcon(Device, metaclass=DeviceMeta):
                 self.updateValue(n)
             except Exception as e:
                 self.warn_stream("update issue: " . str(e))
-        self.debug_stream("finished update of all values, took: " + (time.time() - start_update) + "s")
+        self.debug_stream("finished update of all values, took: " + str(time.time() - start_update) + "s")
         self.last_update = time.time()
         self.syncing = False
 
